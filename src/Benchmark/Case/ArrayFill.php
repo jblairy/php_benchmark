034c3afe034a41fc052604a9d59526ae61662801
jblairy <julien@blairy.fr>
2025-07-09 21:49:13 +0200

    feat: add all attribute to match all php versions

--- src/Benchmark/Case/ArrayFill.php
+++ src/Benchmark/Case/ArrayFill.php
@@ -5,31 +5,11 @@ declare(strict_types=1);
 namespace Jblairy\PhpBenchmark\Benchmark\Case;
 
 use Jblairy\PhpBenchmark\Benchmark\AbstractBenchmark;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php56;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php70;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php71;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php72;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php73;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php74;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php80;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php81;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php82;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php83;
-use Jblairy\PhpBenchmark\PhpVersion\Attribute\Php84;
+use Jblairy\PhpBenchmark\PhpVersion\Attribute\All;
 
 final class ArrayFill extends AbstractBenchmark
 {
-    #[Php56]
-    #[Php70]
-    #[Php71]
-    #[Php72]
-    #[Php73]
-    #[Php74]
-    #[Php80]
-    #[Php81]
-    #[Php82]
-    #[Php83]
-    #[Php84]
+    #[All]
     public function execute(): void
     {
         $arr = array_fill(0, 100000, 'test');
---
+++ src/PhpVersion/Attribute/All.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jblairy\PhpBenchmark\PhpVersion\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+final class All
+{
+}
