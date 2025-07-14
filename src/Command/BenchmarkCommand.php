bb68ce04f2ec3d0468d1a9e9ce5d62904cbf0159
jblairy <julien@blairy.fr>
2025-07-14 12:05:06 +0200

    chore: cleanup command and add option attributes

--- src/Command/BenchmarkCommand.php
+++ src/Command/BenchmarkCommand.php
@@ -8,77 +8,91 @@ use Jblairy\PhpBenchmark\Benchmark\Runner\Benchmark\BenchmarkRunner;
 use Jblairy\PhpBenchmark\Benchmark\Runner\Benchmark\Configurator\Configurator;
 use Jblairy\PhpBenchmark\PhpVersion\Enum\PhpVersion;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Serializer\Encoder\CsvEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[AsCommand(name: 'benchmark:run', description: 'Execute a benchmark')]
-final class BenchmarkCommand extends Command
+final class BenchmarkCommand
 {
     public function __construct(
         private Configurator $configurator,
         private SerializerInterface $serializer,
     ) {
-        parent::__construct();
     }
 
     public function __invoke(
-        InputInterface $input,
         SymfonyStyle $io,
+        #[Option(description: 'Name of  the test to run', name: 'test')]
+        ?string $test = null,
+        #[Option(description: 'Number of iterations to run', name: 'iterations')]
+        int $iterations = 1,
+        #[Option(description: 'Php version to run', name: 'php-version')]
+        ?string $phpVersion = null,
     ): int {
-        $testName = $input->getOption('test');
-        if (is_string($testName) && '' !== $testName) {
-            $this->configurator->setBenchmarkName($testName);
-        }
+        $this->configure($test, $iterations, $phpVersion);
 
-        $iterations = $input->getOption('iterations');
-        if (is_string($iterations) && '' !== $iterations) {
-            $this->configurator->setIterations($iterations);
-        }
-
-        $phpVersion = $input->getOption('php-version');
-        if (is_string($phpVersion) && '' !== $phpVersion) {
-            $this->configurator->setPhpVersion(PhpVersion::from($phpVersion));
-        }
-
-        if (1 !== count($this->configurator->getPhpVersion())) {
+        if (1 < count($this->configurator->getPhpVersion()) || 1 < count($this->configurator->getBenchmarks())) {
             $io->title('🚀 PHP Benchmark Runner');
-            $processes = [];
-            if (file_exists('benchmark.csv')) {
-                unlink('benchmark.csv');
-            }
-            if (file_exists('debug.log')) {
-                unlink('debug.log');
-            }
-
-            foreach ($this->configurator->getBenchmarks() as $benchmark) {
-                foreach ($this->configurator->getPhpVersion() as $phpVersion) {
-                    $benchmarkName = explode('/', $benchmark::class);
-                    $process = new Process(['php', 'bin/console', 'benchmark:run', '--test=' . end($benchmarkName), '--php-version=' . $phpVersion->value, '--iterations=' . $this->configurator->getIterations()]);
-                    $process->start();
-                    $processes[] = $process;
-                }
-            }
-
-            foreach ($processes as $process) {
-                while ($process->isRunning()) {
-                    // TODO log
-                }
-
-                if (!$process->isSuccessful()) {
-                    echo 'Error : ' . $process->getErrorOutput();
-                }
-            }
-
+            $this->createAndRunMultipleProcess();
             $io->success('Benchmark finished.');
 
             return Command::SUCCESS;
         }
 
+        $this->executeBenchmarkRunnerAndAppendToCsv();
+
+        return Command::SUCCESS;
+    }
+
+    private function configure(?string $test, int $iterations, ?string $phpVersion): void
+    {
+        if (null !== $test) {
+            $this->configurator->setBenchmarkName($test);
+        }
+
+        $this->configurator->setIterations($iterations);
+
+        if (null !== $phpVersion) {
+            $this->configurator->setPhpVersion(PhpVersion::from($phpVersion));
+        }
+    }
+
+    private function createAndRunMultipleProcess(): void
+    {
+        $processes = [];
+        if (file_exists('benchmark.csv')) {
+            unlink('benchmark.csv');
+        }
+        if (file_exists('debug.log')) {
+            unlink('debug.log');
+        }
+
+        foreach ($this->configurator->getBenchmarks() as $benchmark) {
+            foreach ($this->configurator->getPhpVersion() as $phpVersion) {
+                $benchmarkName = explode('/', $benchmark::class);
+                $process = new Process(['php', 'bin/console', 'benchmark:run', '--test=' . end($benchmarkName), '--php-version=' . $phpVersion->value, '--iterations=' . $this->configurator->getIterations()]);
+                $process->start();
+                $processes[] = $process;
+            }
+        }
+
+        foreach ($processes as $process) {
+            while ($process->isRunning()) {
+                // TODO log
+            }
+
+            if (!$process->isSuccessful()) {
+                echo 'Error : ' . $process->getErrorOutput();
+            }
+        }
+    }
+
+    private function executeBenchmarkRunnerAndAppendToCsv(): void
+    {
         $runner = new BenchmarkRunner($this->configurator);
         $runner->execute();
 
@@ -87,15 +101,5 @@ final class BenchmarkCommand extends Command
             $this->serializer->serialize($runner->getResults(), CsvEncoder::FORMAT),
             flags: FILE_APPEND,
         );
-
-        return Command::SUCCESS;
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->addOption('test', null, InputOption::VALUE_REQUIRED, 'Name of the test to run', '')
-            ->addOption('iterations', null, InputOption::VALUE_OPTIONAL, 'Number of iterations to run', '')
-            ->addOption('php-version', null, InputOption::VALUE_OPTIONAL, 'Php version to run', '');
     }
 }
