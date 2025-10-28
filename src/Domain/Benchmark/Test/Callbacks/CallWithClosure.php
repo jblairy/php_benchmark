5ee42546b5e489fa6699f79153fc22e3d802efb0
jblairy <julien@blairy.fr>
2025-10-28 13:07:54 +0100

    refactor: improve Mercure progress controller and clean benchmarks

--- src/Domain/Benchmark/Test/Callbacks/CallWithClosure.php
+++ src/Domain/Benchmark/Test/Callbacks/CallWithClosure.php
@@ -5,11 +5,23 @@ declare(strict_types=1);
 namespace Jblairy\PhpBenchmark\Domain\Benchmark\Test\Callbacks;
 
 use Jblairy\PhpBenchmark\Domain\Benchmark\Contract\AbstractBenchmark;
-use Jblairy\PhpBenchmark\Domain\PhpVersion\Attribute\Php70;
+use Jblairy\PhpBenchmark\Domain\PhpVersion\Attribute\Php74;
+use Jblairy\PhpBenchmark\Domain\PhpVersion\Attribute\Php80;
+use Jblairy\PhpBenchmark\Domain\PhpVersion\Attribute\Php81;
+use Jblairy\PhpBenchmark\Domain\PhpVersion\Attribute\Php82;
+use Jblairy\PhpBenchmark\Domain\PhpVersion\Attribute\Php83;
+use Jblairy\PhpBenchmark\Domain\PhpVersion\Attribute\Php84;
+use Jblairy\PhpBenchmark\Domain\PhpVersion\Attribute\Php85;
 
 final class CallWithClosure extends AbstractBenchmark
 {
-    #[Php70]
+    #[Php74]
+    #[Php80]
+    #[Php81]
+    #[Php82]
+    #[Php83]
+    #[Php84]
+    #[Php85]
     public function execute(): void
     {
         for ($i = 0; 100000 > $i; ++$i) {
